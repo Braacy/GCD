dfa204c924a1e9754c92a583c78e0f57b50b5c41
Vladislav Bliznyuk <braacy@yandex.ru>
2025-09-13 19:38:54 +0200

    Initial commit

---
+++ GCD/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Владислав Близнюк on 13.09.2025.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+ 
+}
