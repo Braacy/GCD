6609ad0e7421092309fb7f31a31aa464c949b975
Vladislav Bliznyuk <braacy@yandex.ru>
2025-09-15 14:51:39 +0200

    added alert and asyncAfter for him

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -30,6 +30,37 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchImage()
+        delay(3) {
+            self.alertLogin()
+        }
+    }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+            
+    }
+    
+    fileprivate func alertLogin() {
+        let ac = UIAlertController(title: "Зарегестрированы?", message: "Введите логин и пароль", preferredStyle: .alert)
+        
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        ac.addAction(okAction)
+        ac.addAction(cancelAction)
+       
+        ac.addTextField { (usernameTextField) in
+            usernameTextField.placeholder = "add username"
+        }
+     
+        ac.addTextField { (userPasswordTF) in
+            userPasswordTF.placeholder = "add password"
+            userPasswordTF.isSecureTextEntry = true
+        }
+        self.present(ac, animated: true)
     }
     
 fileprivate func fetchImage() {
@@ -38,8 +69,8 @@ fileprivate func fetchImage() {
     activityIndicato.startAnimating()
     
     let queue = DispatchQueue.global(qos: .utility)
-    queue.async { [self] in
-        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+    queue.async {
+        guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
         DispatchQueue.main.async {
             self.image = UIImage(data: imageData)
            }
